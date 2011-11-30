a7957662f1c95cd117722423d7f3c28da85f8d80
arne182 <arneschwarck@gmail.com>
2011-11-30 14:49:34 +0100

    Add Stop Start

--- leo-gps.c
+++ leo-gps.c
@@ -1334,10 +1334,10 @@ static int gps_set_position_mode(GpsPositionMode mode, int fix_frequency) {
         fix_frequency = 8;
     }
     // fix_frequency is only used by NMEA version
-    s->fix_freq = -1;
-    s->init == STATE_INIT;
+    s->fix_freq = 1;
+    gps_stop();
     usleep(500000);
-    s->init == STATE_START;
+    gps_start();
     s->fix_freq = fix_frequency;
     return 0;
 }
